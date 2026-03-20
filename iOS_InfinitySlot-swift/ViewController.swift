8ae5818f6913899b6ceb36e575e2617c62933782
Phil <irons7654321@gmail.com>
2026-03-20 20:15:46 +0800

    Initial Commit

---
+++ iOS_InfinitySlot-swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_InfinitySlot-swift
+//
+//  Created by Phil on 2026/3/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iOS_InfinitySlot-swiftTests/iOS_InfinitySlot_swiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_InfinitySlot_swiftTests.swift
+//  iOS_InfinitySlot-swiftTests
+//
+//  Created by Phil on 2026/3/20.
+//
+
+import Testing
+@testable import iOS_InfinitySlot_swift
+
+struct iOS_InfinitySlot_swiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
